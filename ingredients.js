8b84bed9dc8b2a74b64ba484f0d0d9b370a67924
Naman Dhalla <namandhalla2261994@gmail.com>
2022-04-02 18:58:11 +0000

    3 Loops combination

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let z = 0;
+while ( z < ingredients.length) {
+  console.log(ingredients[z]);
+  z++;
+};
+
+
+for (let i = 0; i < ingredients.length; i++ ) {
+  console.log(ingredients[i]);
+};
+
+
+for (let k = 0; k < ingredients.length; k++ ) {
+  let reversed = ingredients.reverse();
+   console.log(reversed[k]);
+ };
+ 
+
+
+
